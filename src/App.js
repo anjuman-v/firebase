aecac18f652282e13bc8000f478bcb78dda509fd
anjuman-v <ansa.anjuman00786@gmail.com>
2022-09-07 09:31:06 +0530

    1-1

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Register from './components/Register';
+
+function App() {
+  return (
+    <div className="Appfile">
+      <Register />
+    </div>
+  );
+}
+
+export default App;
